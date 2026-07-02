5fe26305c13d90bd1144a8e072dd22501f7abf75
Marvin Dewitt <mgeoffdewitt@gmail.com>
2026-07-02 08:50:18 -0400

    Refactor account repository to use BaseRepository

--- backend/app/repositories/account_repository.py
+++ backend/app/repositories/account_repository.py
@@ -1,22 +1,10 @@
 from sqlalchemy.orm import Session
 
 from app.models.account import Account
+from app.repositories.base_repository import BaseRepository
 from app.schemas.account import AccountCreate
 
 
-class AccountRepository:
+class AccountRepository(BaseRepository[Account, AccountCreate]):
     def __init__(self, db: Session):
-        self.db = db
-
-    def create(self, data: AccountCreate) -> Account:
-        record = Account(**data.model_dump())
-        self.db.add(record)
-        self.db.commit()
-        self.db.refresh(record)
-        return record
-
-    def list_all(self) -> list[Account]:
-        return self.db.query(Account).filter(Account.is_active == True).all()
-
-    def get_by_id(self, record_id: str) -> Account | None:
-        return self.db.query(Account).filter(Account.id == record_id).first()
+        super().__init__(db, Account)
