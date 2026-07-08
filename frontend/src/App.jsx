f915f7610abd2c15665ba6228c07a0119cfb5a8a
Marvin Dewitt <mgeoffdewitt@gmail.com>
2026-07-08 12:52:27 -0400

    Add analyst workspace page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./layout/Layout";
 import ExecutiveDashboard from "./pages/ExecutiveDashboard";
 import IdentityIntelligence from "./pages/IdentityIntelligence";
 import IdentityExplorer from "./pages/IdentityExplorer";
+import AnalystWorkspace from "./pages/AnalystWorkspace";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<ExecutiveDashboard />} />
         <Route path="/identity/:id" element={<IdentityIntelligence />} />
         <Route path="/explorer/:identityId" element={<IdentityExplorer />} />
+        <Route path="/workspace/:identityId" element={<AnalystWorkspace />} />
       </Routes>
     </Layout>
   );
