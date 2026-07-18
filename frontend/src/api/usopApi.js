b8c5b7710670dc3f87211cd80253b468dad2fda4
Marvin Dewitt <mgeoffdewitt@gmail.com>
2026-07-17 23:02:44 -0400

    Make frontend API base URL configurable

--- frontend/src/api/usopApi.js
+++ frontend/src/api/usopApi.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const apiBaseUrl = (
+  import.meta.env.VITE_API_BASE_URL
+  || "http://127.0.0.1:8000"
+).replace(/\/$/, "");
+
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+  baseURL: apiBaseUrl,
 });
 
 export default api;
